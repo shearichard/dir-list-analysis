a48569048a60d3a6aa3150ee7d21352d5d3f5877
Richard Shea <rshea@thecubagroup.com>
2014-08-06 14:29:16 +1200

    Snapshotting a few trivial arg handling changes

--- dla/whats_present.py
+++ dla/whats_present.py
@@ -19,6 +19,15 @@ Options:
 from docopt import docopt
 
 
+def main(filein, filetest, fileout):
+    print filein
+    print filetest
+    print fileout
+
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='Whats Present 0.1')
+    import sys
+    print sys.argv[1]
+    print sys.argv[2]
     print(arguments)
+    main(arguments['-i'], arguments['-t'], arguments['-o'])
