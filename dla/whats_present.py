f45e85b88361a86bfef2c16ae13dd600da59b72d
Richard Shea <rshea@thecubagroup.com>
2014-08-06 13:35:04 +1200

    Arg parsing now in place

--- dla/whats_present.py
+++ dla/whats_present.py
@@ -5,4 +5,20 @@ Given a list of file names provides information about which files are
 present and which are absent from within a file listing. There's a good
 number of tools out there that will do this as well but in my case I didn't
 have access to those.
+
+Usage:
+  whats_present.py [-h] -i FILEDIR -o FILEOUT -t FILETEST
+
+Options:
+    -h --help    show this
+    -i FILEDIR   specify directory listing file 
+    -o FILEOUT   specify output file 
+    -t FILETEST  specify test file 
+
 '''
+from docopt import docopt
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='Whats Present 0.1')
+    print(arguments)
