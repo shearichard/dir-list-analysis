3bf92c98ad54e1fa43a3b3dfea33678a4c7c724c
Richard Shea <rshea@thecubagroup.com>
2014-08-07 17:20:13 +1200

    Process input file and put relevant bits into a dictionary - complete and working

--- dla/whats_present.py
+++ dla/whats_present.py
@@ -18,11 +18,62 @@ Options:
 '''
 from docopt import docopt
 
+class DirectoryListing(object):
+    def __init__(self, fileinpath):
+        self.fileinpath = fileinpath 
+        self.dicfiles = {}
+        self.__make_dic_from_filein()
 
+    def __make_dic_from_fileinline(self, line):
+        '''
+        Given an input line from the file
+        add an element to the instance var dict
+        which corresponds to one file in the listing
+        '''
+        lstelems = line.split()
+        try:
+            mm, dd, yyyy  = lstelems[0].split("/")
+        except ValueError:
+            pass
+        except IndexError:
+            pass
+        else:
+            str_dt_iso = "%s-%s-%s" % (yyyy, mm, dd) 
+            
+            #file names can have spaces (unfortunaly)
+            filename = " ".join(lstelems[4:])
+            #deal with commas in size
+            if lstelems[3] == """<DIR>""":
+                pass
+            else:
+                size = int(lstelems[3].replace(',',''))
+                #not sure what to use as key, use file name for now
+                key = filename
+                
+                self.dicfiles[key] = {'filename': filename, 'size': size, 'dateiso' : str_dt_iso} 
+        
+    def __make_dic_from_filein(self):
+        '''
+        Based on a file which has at least one line which looks like this:
+
+        12/15/2003  01:15 PM             3,383 000BE98E-7076-4CF8-8BFA-266E6EE4C9DF.GIF
+
+        a dict of dicts is returned, keyed on file name.
+        '''
+        lstOut = []
+        with open(self.fileinpath, 'r') as f:
+            for line in f:
+                self.__make_dic_from_fileinline(line)
+    
 def main(filein, filetest, fileout):
     print filein
     print filetest
     print fileout
+    d = DirectoryListing(filein)
+    import pprint
+    pprint.pprint(d.dicfiles)
+    print len(d.dicfiles)
+    print filein
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='Whats Present 0.1')
